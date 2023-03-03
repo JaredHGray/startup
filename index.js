f6dedbde43b60662bb4740393e1f61e60c9ac02e
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2023-03-03 10:20:47 -0700

    created function to shuffle and display questions

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
    element.style.visibility = 'hidden'; //on load of screen hide normal HTML
    document.body.style.backgroundImage = 'none';
    countjob;
+   DisplayQuestion(0);
 });
 
 function countDown() {
@@ -24,3 +25,42 @@ function countDown() {
     }
 }
 //end of countdown code
+
+//object to store questions 
+const questions = [
+    {
+    question: "How many hearts does an Octopus have?",
+    optionA: "One",
+    optionB: "Two",
+    optionC: "Three",
+    optionD: "Four",
+    correctOption: "optionC"
+    }
+]
+
+let shuffledQuestions = [] //empty array to shuffle questions
+
+function questionOrder() { //shuffles the questions and stores them in the shuffledQuestions array
+    while(shuffledQuestions.length <= 0){
+        const random = questions[Math.floor(Math.random() * questions.length)];
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+let questionNumber = 1; //holds current question number
+let playerScore = 0; //holds player score
+let wrongAnswer = 0; //holds #wrong answers by player
+let indexNumber = 0; //used in displaying next question
+
+function DisplayQuestion(index) {
+    questionOrder();
+    const currentQuestion = shuffledQuestions[index];
+    document.getElementById('questionNumber').innerHTML = questionNumber;
+    document.getElementById('displayQuestion').innerHTML = currentQuestion.question;
+    document.getElementById('option1Label').innerHTML = currentQuestion.optionA;
+    document.getElementById('option2Label').innerHTML = currentQuestion.optionB;
+    document.getElementById('option3Label').innerHTML = currentQuestion.optionC;
+    document.getElementById('option4Label').innerHTML = currentQuestion.optionD;
+}
