7e7413d398ec6b69285c0fc93802dfabcda204d8
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2023-03-26 14:24:32 -0600

    added in user functions for DB

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -12,7 +14,9 @@ const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
 const scoreCollection = client.db('startup').collection('scores');
+const userCollection = client.db('startup').collection('user');
 
+/*functions for scores*/
 function addScore(score) {
     scoreCollection.insertOne(score);
   }
@@ -26,5 +30,28 @@ function addScore(score) {
     const cursor = scoreCollection.find(query, options);
     return cursor.toArray();
   }
+
+/*functions for user info*/
+function getUser(userName) {
+  return userCollection.findOne({ userName: userName });
+}
+
+function getUserByToken(token){
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(userName, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    userName: userName,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
   
-  module.exports = {addScore, getHighScores};
+  module.exports = {addScore, getHighScores, getUser, getUserByToken, createUser};
